6a3b4d49e62e08e97009ab63507e92b506c6c9dc
Avinash Kumar Shukla <avinashshukla.dev@gmail.com>
2025-08-21 02:09:40 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -31,6 +31,7 @@ const App = () => (
         <Toaster />
         <Sonner />
         <HashRouter>
+          <Header />
             <ScrollToTop />
             <Routes>
               <Route path="/" element={<Index />} />
@@ -51,6 +52,7 @@ const App = () => (
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
             </Routes>
+          <Footer />
         </HashRouter>
       </TooltipProvider>
     </AuthProvider>
