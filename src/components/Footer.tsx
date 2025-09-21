65190da6b1d16bc15395068b27d1a23a17012968
TanyaVaish-17 <tanyavaish05@gmail.com>
2025-09-21 22:24:09 +0530

    Updated Whatshapp Link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,7 +18,7 @@ const Footer = () => {
       { name: "Discord", href: "https://discord.gg/HuBa9r33kW" },
       { name: "Instagram", href: "https://www.instagram.com/zenyukti/" },
       { name: "LinkedIn", href: "https://linkedin.com/company/zenyukti" },
-      { name: "Whatsapp", href: "https://chat.whatsapp.com/HTwSFGYUNIb6m75IAzCxfk?mode=ac_t" },
+      { name: "Whatsapp", href: "https://chat.whatsapp.com/B4zVLDm9frx92REjI9d4gO?mode=ems_copy_c" },
     ],
     resources: [
       //{ name: "Documentation", href: "#" },
